769aeb60b858cae21c7ff5f7a18e7441e8463fae
bust3902 <bust3902@gmail.com>
2022-06-20 12:28:56 +0900

    업데이트

---
+++ src/main/java/com/htabooks/vo/CartItem.java
@@ -0,0 +1,37 @@
+package com.htabooks.vo;
+
+import java.util.Date;
+
+public class CartItem {
+	int no;
+	int userNo;
+	int bookNo;
+	Date createdDate;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public int getBookNo() {
+		return bookNo;
+	}
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	
+	
+}
